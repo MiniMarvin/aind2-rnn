230b16125f6b6e0b2292d47dbf48c36433a5a161
Caio Gomes <caiogomes34@gmail.com>
2018-08-08 17:03:32 -0300

    Completed the first 3 tasks in the repository

--- my_answers.py
+++ my_answers.py
@@ -8,10 +8,18 @@ import keras
 
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_series(series, window_size):
+def window_transform_series(series, window_size=5):
     # containers for input/output pairs
     X = []
     y = []
+
+    for x in range(0, len(series) - window_size):
+        buff = np.array(series[x:x+window_size])
+        X.append(buff)
+        y.append(np.array([series[x+window_size]]))
+
+    X = np.array(X)
+    y = np.array(y)
 
     # reshape each 
     X = np.asarray(X)
@@ -23,12 +31,19 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1), return_sequences=False))
+    model.add(Dense(1))
+
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+
+    for p in punctuation:
+        text = text.replace(p, ' ')
 
     return text
 
@@ -38,9 +53,17 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for x in range(0, len(text) - window_size - 1, step_size):
+        buff = text[x:x+5]
+        inputs.append(buff)
+        outputs.append(text[x+5])
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars, activation="softmax"))
+    return model
